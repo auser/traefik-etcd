a854f84ded90e3f9e6c140ceca81e6c24cf67fd6
Ari <me@ari.io>
2024-12-03 20:35:20 -0800

    Updates to generated types

--- generated/types/index.ts
+++ generated/types/index.ts
@@ -1,77 +1,9 @@
-export interface PathConfig {
-    path: string;
-    deployments: Record<string, DeploymentConfig>;
-    middlewares: string[];
-    stripPrefix: boolean;
-    passThrough: boolean;
+export interface PartialEtcdConfig {
+    endpoints?: string[] | undefined;
+    timeout?: number | undefined;
+    keepAlive?: number | undefined;
+    tls?: TlsOptions | undefined;
 }
-
-export interface DeploymentConfig {
-    name: string;
-    ip: string;
-    port: number;
-    weight: number;
-    selection?: SelectionConfig | undefined;
-    protocol: DeploymentProtocol;
-    middlewares?: string[] | undefined;
-}
-
-export interface FromClientIpConfig {
-    range?: string | undefined;
-    ip?: string | undefined;
-}
-
-export interface HeadersConfig {
-    customRequestHeaders: Record<string, string>;
-    customResponseHeaders: Record<string, string>;
-    accessControlAllowMethods: string[];
-    accessControlAllowHeaders: string[];
-    accessControlExposeHeaders: string[];
-    accessControlAllowOriginList: string[];
-    addVaryHeader: boolean;
-}
-
-export interface SelectionConfig {
-    withCookie?: WithCookieConfig | undefined;
-    fromClientIp?: FromClientIpConfig | undefined;
-}
-
-export interface MiddlewareConfig {
-    name: string;
-    headers?: HeadersConfig | undefined;
-    protocol: string;
-}
-
-export interface HealthCheckConfig {
-    path: string;
-    interval: string;
-    timeout: string;
-}
-
-export interface WithCookieConfig {
-    name: string;
-    value?: string | undefined;
-}
-
-export interface TraefikConfig {
-    rulePrefix: string;
-    etcd: EtcdConfig;
-    hosts: HostConfig[];
-    middlewares: Record<string, MiddlewareConfig>;
-}
-
-export interface TlsOptions {
-    domain?: string | undefined;
-    cert?: string | undefined;
-    key?: string | undefined;
-    ca?: string | undefined;
-}
-
-export type DeploymentProtocol =
-    | "Http"
-    | "Https"
-    | "Tcp"
-    | "Invalid";
 
 export interface EtcdConfig {
     endpoints: string[];
@@ -88,10 +20,78 @@ export interface HostConfig {
     selection?: SelectionConfig | undefined;
 }
 
-export interface PartialEtcdConfig {
-    endpoints?: string[] | undefined;
-    timeout?: number | undefined;
-    keepAlive?: number | undefined;
-    tls?: TlsOptions | undefined;
+export interface PathConfig {
+    path: string;
+    deployments: Record<string, DeploymentConfig>;
+    middlewares: string[];
+    stripPrefix: boolean;
+    passThrough: boolean;
+}
+
+export interface WithCookieConfig {
+    name: string;
+    value?: string | undefined;
+}
+
+export interface FromClientIpConfig {
+    range?: string | undefined;
+    ip?: string | undefined;
+}
+
+export type DeploymentProtocol =
+    | "Http"
+    | "Https"
+    | "Tcp"
+    | "Invalid";
+
+export interface TraefikConfig {
+    rulePrefix: string;
+    etcd: EtcdConfig;
+    hosts: HostConfig[];
+    middlewares: Record<string, MiddlewareConfig>;
+}
+
+export interface SelectionConfig {
+    withCookie?: WithCookieConfig | undefined;
+    fromClientIp?: FromClientIpConfig | undefined;
+}
+
+export interface HealthCheckConfig {
+    path: string;
+    interval: string;
+    timeout: string;
+}
+
+export interface MiddlewareConfig {
+    name: string;
+    headers?: HeadersConfig | undefined;
+    protocol: string;
+}
+
+export interface HeadersConfig {
+    customRequestHeaders: Record<string, string>;
+    customResponseHeaders: Record<string, string>;
+    accessControlAllowMethods: string[];
+    accessControlAllowHeaders: string[];
+    accessControlExposeHeaders: string[];
+    accessControlAllowOriginList: string[];
+    addVaryHeader: boolean;
+}
+
+export interface TlsOptions {
+    domain?: string | undefined;
+    cert?: string | undefined;
+    key?: string | undefined;
+    ca?: string | undefined;
+}
+
+export interface DeploymentConfig {
+    name: string;
+    ip: string;
+    port: number;
+    weight: number;
+    selection?: SelectionConfig | undefined;
+    protocol: DeploymentProtocol;
+    middlewares?: string[] | undefined;
 }
 
